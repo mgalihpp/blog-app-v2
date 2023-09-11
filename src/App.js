9d2c72ffc3a9cd0a5ef08fe530159f9bf26d2c74
mgalihpp <muhammadgalih451@gmail.com>
2023-09-11 16:51:43 +0700

    add route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import Navbar from "./components/Navbar/Navbar";
-import Home from './pages/home/Home';
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import Settings from "./pages/Settings/Settings";
+import Write from "./pages/Write/Write";
+import Home from "./pages/home/Home";
+import Single from "./pages/single/Single";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-      <Home/>
-    </>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/register" element={<Register/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/write" element={<Write/>} />
+        <Route path="/settings" element={<Settings/>} />
+        <Route path="/post/:postId" element={<Single/>} />
+      </Routes>
+    </Router>
   );
 }
 

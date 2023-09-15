4306ee808eefcb9c1e1af7f62879fb723c49cf0c
mgalihpp <muhammadgalih451@gmail.com>
2023-09-16 01:11:54 +0700

    add context

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useContext } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
@@ -5,19 +6,36 @@ import Settings from "./pages/Settings/Settings";
 import Write from "./pages/Write/Write";
 import Home from "./pages/home/Home";
 import Single from "./pages/single/Single";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { Context } from "./context/Context";
 
 function App() {
+  const { user } = useContext(Context);
+
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/register" element={<Register/>} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/write" element={<Write/>} />
-        <Route path="/settings" element={<Settings/>} />
-        <Route path="/post/:postId" element={<Single/>} />
+        <Route path="/" element={user ? <Home /> : <Navigate to="/login" />} />
+        <Route
+          path="/register"
+          element={user ? <Navigate to="/" /> : <Register />}
+        />
+        <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
+        <Route
+          path="/write"
+          element={user ? <Write /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/settings"
+          element={user ? <Settings /> : <Navigate to="/login" />}
+        />
+        <Route path="/post/:postId" element={<Single />} />
       </Routes>
     </Router>
   );
